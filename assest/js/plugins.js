ac30d81b373ba667a890c0e98cb3130aff567caf
ahmed abdellatif <mokaworld2023@gmail.com>
2024-01-08 19:51:02 +0200

    update files

--- assest/js/plugins.js
+++ assest/js/plugins.js
@@ -1,34 +1,39 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assest/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
-
-// INIT OWL CAROUSEL 
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:1,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    pagination: false,
-    rtl: true,
-    dots:false,
-    autoplayTimeout:2500,
-    autoplayHoverPause:true
+particlesJS.load("particles-js", "assest/particles.json", function () {
+  console.log("callback - particles.js config loaded");
 });
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[2500])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
 
+// INIT OWL CAROUSEL
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  items: 1,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  pagination: false,
+  rtl: true,
+  dots: false,
+  autoplayTimeout: 2500,
+  autoplayHoverPause: true,
+});
+$(".play").on("click", function () {
+  owl.trigger("play.owl.autoplay", [2500]);
+});
+$(".stop").on("click", function () {
+  owl.trigger("stop.owl.autoplay");
+});
 
-// loader 
-let loader = document.getElementById('loader');
+// loader
+let loader = document.getElementById("loader");
 
-window.onload = function(){
-    loader.style.display = "none";
-    console.log("loading successfully ");
-    
+window.onload = function () {
+  loader.style.display = "none";
+  console.log("loading successfully ");
 };
+
+// loading page
+
+setInterval(function loadPage() {
+  let loader = document.getElementById("loader");
+  loader.style.display = "none";
+});
